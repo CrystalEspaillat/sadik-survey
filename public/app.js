ab729d49b9df3fb945b94846506ff4fc1e8fbc9d
CrystalEspaillat <crystalespaillat@Crystals-MBP.home>
2019-02-01 01:46:14 -0500

    Started layout

---
+++ public/app.js
@@ -0,0 +1,46 @@
+$(document).ready(() => {
+
+    // Click Events
+
+    $("#collect-contact").on("click", () => {
+        generateExpModal();
+    });
+
+    let generateExpModal = () => {
+        $("#survey").append("html", "<div class='question';> </div>");
+        
+    };
+
+    $("#collect-experience").on("click", () => {
+
+    });
+
+
+    $("#collect-lifestyle").on("click", () => {
+
+    });
+
+    $("#submit-form").on("click", () => {
+
+        // Collect full name, last name, and email.
+        firstName = $("#first-name");
+        lastName = $("#last-name");
+        email = $("#email");
+
+        // Collect experience value.
+
+        // Collect lifestyle range value. 
+
+        // Collect competition response
+    
+        // collect textarea input
+    
+        });
+
+    // Load Modals
+        let generateExpModal = () => {
+            $("#survey").append("html", "<div class='question'> <h1> Have you ever had an online coach before? </h1> </div>");
+            
+        };
+
+});
