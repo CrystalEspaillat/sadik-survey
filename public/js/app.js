dd145504a827b94a4ef4d388bf77eeba58cb6c7f
CrystalEspaillat <crystalespaillat@Crystals-MacBook-Pro.local>
2019-02-02 09:49:46 -0500

    Added backend

---
+++ public/js/app.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+    // Click Events
+
+    $('#start-survey').on('click', () => {
+
+    });
+
+    // $("#collect-contact").on("click", () => {
+   
+    // });
+
+    // $("#collect-experience").on("click", () => {
+
+    // });
+
+
+    // $("#collect-lifestyle").on("click", () => {
+
+    // });
+
+    // $("#submit-form").on("click", () => {
+
+    //     // Collect full name, last name, and email.
+    //     firstName = $("#first-name");
+    //     lastName = $("#last-name");
+    //     email = $("#email");
+
+    //     // Collect experience value.
+
+    //     // Collect lifestyle range value. 
+
+    //     // Collect competition response
+    
+    //     // collect textarea input
+    
+    //     });
+
+});
