0b3f20405b1b79c6eb72886874b5a2b15373fcb2
CrystalEspaillat <crystalespaillat@Crystals-MacBook-Pro.local>
2019-02-06 13:04:28 -0500

    Finished

--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,129 @@
 $(document).ready(function() {
 
-    // Click Events
-
-    $('#start-survey').on('click', () => {
-
+    $("#product1").on("click", () => {
+        let url = "https://www.coachingwithsadik.com/products/4-week-diet-training"
+        window.open(url);
     });
 
-    // $("#collect-contact").on("click", () => {
-   
-    // });
+    $("#product2").on("click", () => {
+        let url = "https://www.coachingwithsadik.com/products/8-week-diet-training"
+        window.open(url);
+    });
 
-    // $("#collect-experience").on("click", () => {
+    $("#product3").on("click", () => {
+        let url = "https://www.coachingwithsadik.com/products/12-week-diet-training"
+        window.open(url);
+    });
 
-    // });
 
+    // Real-time Validation
 
-    // $("#collect-lifestyle").on("click", () => {
+        // First Name Validation
+        $("#form-first-name").on("input", () => {
+            let input = $("#form-first-name");
+            let firstName = input.val();
+            if (firstName) {input.removeClass("invalid").addClass("valid");} 
+            else {input.removeClass("valid").addClass("invalid");}
+        });
 
-    // });
+        // Last Name Validation
+        $("#form-last-name").on("input", () => {
+            let input = $("#form-last-name");
+            let lastName = input.val();
+            if (lastName) {input.removeClass("invalid").addClass("valid");} 
+            else {input.removeClass("valid").addClass("invalid");}
+        });
+        
+        // Email Validation
+        $("#form-email").on("input", () => {
+            let input = $("#form-email");
+            let re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+            let formEmail = re.test(input.val());
+            if (formEmail) {input.removeClass("invalid").addClass("valid");} 
+            else {input.removeClass("valid").addClass("invalid");}
+        });        
+        
+        // Goals Validation
+        $("#form-goals").keyup((event) => {
+            let input = $("#form-goals");
+            let goals = input.val();
+            if (goals) {
+                input.removeClass("invalid").addClass("valid");} 
+            else {input.removeClass("valid").addClass("invalid");}
+        });
 
-    // $("#submit-form").on("click", () => {
+    // On-Submit Validation
+    $("#submit-form").on("click", (event) => {        
+        let formData = $("#form").serializeArray();
+        let errorFree = true;
 
-    //     // Collect full name, last name, and email.
-    //     firstName = $("#first-name");
-    //     lastName = $("#last-name");
-    //     email = $("#email");
+        for (let input in formData) {
+            let element = $("#form-" + formData[input]['name']);
+            let valid = element.hasClass("valid");
+            let errorElement = $("span", element.parent());
+            if (!valid) {
+                errorElement.removeClass("error").addClass("error-show"); 
+                errorFree = false;
+            } else { 
+                errorElement.removeClass("error-show").addClass("error");
+            }
+        }
 
-    //     // Collect experience value.
+        // Collect form values
+        let firstName = $("#form-first-name").val().trim();
+        let lastName = $("#form-last-name").val().trim();
+        let email = $("#form-email").val().trim();
+        let experience = $("#form input[type='radio']:checked").val();        
+        let lifestyle = $("#form input[type='range']").val();
+        let goals = $("#form-goals").val();
+        
+        // NOPE
+        if (!errorFree) {
+            event.preventDefault(); 
 
-    //     // Collect lifestyle range value. 
+            $("#error-warning").removeClass("hide-warning").addClass("show-warning");
+        }
 
-    //     // Collect competition response
-    
-    //     // collect textarea input
-    
-    //     });
+        // OKAY 
+        else {
+            event.preventDefault();
+
+            // Data to send via email
+            let newApplication = {
+                firstname: firstName,
+                lastname: lastName,
+                email: email,
+                experience: experience,
+                lifestyle: lifestyle,
+                goals: goals
+            }
+
+            console.log(newApplication);
+          
+            // Send email
+            $.post("/send", newApplication, data => {
+              console.log(newApplication);
+            });
+
+            // Clear form
+            firstName = $("#form-first-name").val("");
+            lastName = $("#form-last-name").val("");
+            email = $("#form-email").val("");
+            goals = $("#form-goals").val("");          
+
+            //Remove validation classes
+            $("#form-first-name").removeClass("valid");
+            $("#form-last-name").removeClass("valid");
+            $("#form-email").removeClass("valid");
+            $("#form-goals").removeClass("valid");
+
+            // Change & disable send button
+            $("#submit-form").text("BOOM!").addClass("sent").attr("disabled", "disabled");
+
+            // Display sent message
+            $(".sent-message").addClass("show-sent-message").removeClass("sent-message");
+
+        }
+    });
 
 });
--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').load();
-// const nodemailer = require('nodemailer');
-// let smtpTransport = require('nodemailer-smtp-transport');
-// const request = require('superagent');
+const nodemailer = require('nodemailer');
+let smtpTransport = require('nodemailer-smtp-transport');
+const request = require('superagent');
 const app = express();
 
 // SERVE STATIC FILES
@@ -23,95 +23,87 @@ app.get('/survey', function(req, res) {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// // CLICK EVENT
-// app.post('/send', (req, res) => {
+// CLICK EVENT
+app.post('/send', (req, res) => {
 
-//     //NODEMAILER
-//     smtpTransport = nodemailer.createTransport( {
-//         service: "Gmail",
-//         host: 'smtp.gmail.com',
-//         port: 465,
-//         secure: true,
-//         auth: {
-//             user: process.env.EMAILSENDS,
-//             pass: process.env.USERPASSWORD
-//         }
-//     });
+    //NODEMAILER
+    smtpTransport = nodemailer.createTransport( {
+        service: "Gmail",
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.EMAILSENDS,
+            pass: process.env.USERPASSWORD
+        }
+    });
 
-//     // Variables holding some form responses
-//    const firstName = `${req.body.firstname}`;
-//    const replyTo = `${req.body.email}`;
-//    const marketing = `${req.body.marketing}`;
+    // Variables holding some form responses
+   const firstName = `${req.body.firstname}`;
+   const replyTo = `${req.body.email}`;
+   const marketing = `${req.body.marketing}`;
 
 
-//     // create a body for the email
-//     const output = `
-//         <hr>
-//         <h1>Contact Details:</h1> 
-//         <p style="margin-left:10px;"><strong>Name:</strong> ${req.body.firstname} ${req.body.lastname}</p>
-//         <p style="margin-left:10px;"><strong>Email:</strong> ${req.body.email}</p>
-//         <p style="margin-left:10px;"><strong>Accepts Marketing:</strong> ${req.body.marketing}</p>
-//         <h1>Goals:</h1>
-//         <p style="margin-left:10px;">${req.body.goals}</p>
-//         <hr>
-//         <p> Please do NOT reply to this unmonitored email address. </p>
-//     `;
+    // create a body for the email
+    const output = `
+        <hr>
+        <h1>Contact Details:</h1> 
+        <p style="margin-left:10px;"><strong>Name:</strong> ${req.body.firstname} ${req.body.lastname}</p>
+        <p style="margin-left:10px;"><strong>Email:</strong> ${req.body.email}</p>
+        <p style="margin-left:10px;"><strong>Worked with online coach before?</strong> ${req.body.experience}</p>
+        <p style="margin-left:10px;"><strong>Active Lifestyle Rating ?</strong> ${req.body.lifestyle}/100</p>
+        <h1>Goals:</h1>
+        <p style="margin-left:10px;">${req.body.goals}</p>
+        <hr>
+        <p> Please do NOT reply to this unmonitored email address. </p>
+    `;
 
-//     // create email
-//     var mailOptions = {
-//         from: process.env.EMAILSENDS,
-//         to: process.env.EMAILGETS,
-//         replyTo: replyTo,
-//         subject: 'New Swipe-Up Inquiry from ' + firstName,
-//         text: 'You have a new client inquiry to review!',
-//         html: output
-//     }
+    // create email
+    var mailOptions = {
+        from: process.env.EMAILSENDS,
+        to: process.env.EMAILGETS,
+        replyTo: replyTo,
+        subject: 'New Swipe-Up Inquiry from ' + firstName,
+        text: 'You have a new client inquiry to review!',
+        html: output
+    }
 
-//     // send the mail
-//     smtpTransport.sendMail(mailOptions, function(error, response) {
-//         if (error) {
-//             console.log(error);
-//         } else {
-//             console.log("Message sent: " + response);
-//         }
-//         smtpTransport.close(); // shut down the connection pool, no more messages
-//     });
+    // send the mail
+    smtpTransport.sendMail(mailOptions, function(error, response) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Message sent: " + response);
+        }
+        smtpTransport.close(); // shut down the connection pool, no more messages
+    });
 
-//     // EMAIL MARKETING 
-//     if (marketing === "yes") {
+    // ADD TO MAILCHIMP
 
-//         // ADD TO MAILCHIMP
+    const mailchimpInstance = process.env.MAILCHIMPINST;
+    const listUniqueId = process.env.MAILCHIMPLIST;
+    const mailchimpApiKey = process.env.MAILCHIMPAPI;
 
-//         const mailchimpInstance = process.env.MAILCHIMPINST;
-//         const listUniqueId = process.env.MAILCHIMPLIST;
-//         const mailchimpApiKey = process.env.MAILCHIMPAPI;
-
-//         request
-//             .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
-//             .set('Content-Type', 'application/json;charset=utf-8')
-//             .set('Authorization', 'Basic ' + new Buffer('any:' + mailchimpApiKey ).toString('base64'))
-//             .send({
-//             'email_address': req.body.email,
-//             'status': 'subscribed',
-//             'merge_fields': {
-//                 'FNAME': req.body.firstname,
-//                 'LNAME': req.body.lastname
-//             }
-//             })
-//             .end(function(err, response) {
-//                 if (response.status < 300 || (response.status === 400 && response.body.title === "Member Exists")) {
-//                 console.log('Signed Up!');
-//                 } else {
-//                 console.log('Sign Up Failed :(');
-//                 }
-//             });
-
-//         } else {
-//             console.log("User has NOT been added to mailing list.")
-//         }
-
-//  });
-
+    request
+        .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
+        .set('Content-Type', 'application/json;charset=utf-8')
+        .set('Authorization', 'Basic ' + new Buffer('any:' + mailchimpApiKey ).toString('base64'))
+        .send({
+        'email_address': req.body.email,
+        'status': 'subscribed',
+        'merge_fields': {
+            'FNAME': req.body.firstname,
+            'LNAME': req.body.lastname
+        }
+        })
+        .end(function(err, response) {
+            if (response.status < 300 || (response.status === 400 && response.body.title === "Member Exists")) {
+            console.log('Signed Up!');
+            } else {
+            console.log('Sign Up Failed :(');
+            }
+        });
+});
 
 // LISTEN
 app.listen(process.env.PORT || 3000, () => console.log('Server started...'));
